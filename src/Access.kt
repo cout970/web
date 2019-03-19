99124407a1ef72b213fdc62273b16dc5c1f5df89
cout970 <thecout970@gmail.com>
2019-03-19 18:49:25 +0100

    Improve project organization

---
+++ src/Access.kt
@@ -0,0 +1,20 @@
+package com.cout970
+
+import io.ktor.application.Application
+import io.ktor.application.ApplicationCall
+import io.ktor.application.log
+import io.ktor.request.host
+import io.ktor.sessions.get
+import io.ktor.sessions.sessions
+
+fun Application.assertAccess(call: ApplicationCall) {
+    if (!checkAccess(call)) {
+        log.warn("(IP: ${call.request.host()}) Attempt to access with invalid key: ${call.sessions.get<SessionKey>()}")
+        throw AuthenticationException()
+    }
+}
+
+fun checkAccess(call: ApplicationCall): Boolean {
+    val session = call.sessions.get<SessionKey>() ?: SessionKey("")
+    return session.key == System.getenv("ktor_private_key")
+}
--- src/Application.kt
+++ src/Application.kt
@@ -1,6 +1,9 @@
 package com.cout970
 
-import io.ktor.application.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.application.log
 import io.ktor.features.*
 import io.ktor.gson.gson
 import io.ktor.http.ContentType
@@ -14,16 +17,73 @@ import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.ShutDownUrl
-import io.ktor.sessions.*
+import io.ktor.sessions.Sessions
+import io.ktor.sessions.cookie
+import io.ktor.sessions.sessions
+import io.ktor.sessions.set
 import org.slf4j.event.Level
 import java.io.File
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+data class SessionKey(val key: String)
+
+class AuthenticationException : RuntimeException()
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    install()
 
+    routing {
+        static("/static") {
+            resources("static")
+        }
+
+        get("/") {
+            call.respond(
+                LocalFileContent(
+                    File("resources/templates/index.html"),
+                    contentType = ContentType.Text.Html
+                )
+            )
+        }
+
+        get("/session") {
+            call.sessions.set(SessionKey(call.parameters["key"] ?: ""))
+            assertAccess(call)
+            call.respondText(
+                """
+                    Login successfully
+                    <a href="/admin_page">Admin page</a>
+                    """.trimIndent(),
+                contentType = ContentType.Text.Html
+            )
+        }
+
+        get("/admin_page") {
+            assertAccess(call)
+            call.respondFile(File("resources/templates/admin.html"))
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world 2"))
+        }
+
+        get("/run/update") {
+            assertAccess(call)
+            this@module.log.info("Starting update checks")
+            call.respondText(updateServer(), contentType = ContentType.Text.Plain)
+            this@module.log.info("Update done")
+        }
+
+        get("/run/restart") {
+            assertAccess(call)
+            ShutDownUrl("") { 1 }.doShutdown(call)
+        }
+    }
+
+}
+
+fun Application.install() {
     install(Sessions) {
         cookie<SessionKey>("SESSION_KEY")
     }
@@ -56,78 +116,15 @@ fun Application.module(testing: Boolean = false) {
         gson()
     }
 
-    routing {
-        static("/static") {
-            resources("static")
-        }
-
-        get("/") {
-            call.respond(LocalFileContent(
-                File("resources/templates/index.html"),
-                contentType = ContentType.Text.Html
-            ))
-        }
-
-        get("/session") {
-            call.sessions.set(SessionKey(call.parameters["key"] ?: ""))
-            if (assertAccess(call)) {
-                call.respondText("""
-                    Login successfully
-                    <a href="/admin_page">Admin page</a>
-                    """.trimIndent(), contentType = ContentType.Text.Html)
-            }
-        }
-
-        get("/admin_page"){
-            if (assertAccess(call)) {
-                call.respondFile(File("resources/templates/admin.html"))
-            }
-        }
-
-        get("/json/gson") {
-            call.respond(mapOf("hello" to "world 2"))
-        }
-
-        get("/run/update") {
-            if (!assertAccess(call)) return@get
-            this@module.log.info("Starting update checks")
-            call.respondText(updateServer(), contentType = ContentType.Text.Plain)
-            this@module.log.info("Update done")
-        }
-
-        get("/run/restart") {
-            if (!assertAccess(call)) return@get
-            ShutDownUrl("") { 1 }.doShutdown(call)
-        }
-
-        install(StatusPages) {
-            exception<AuthenticationException> { cause ->
-                call.respond(HttpStatusCode.Unauthorized)
-            }
-            exception<AuthorizationException> { cause ->
-                call.respond(HttpStatusCode.Forbidden)
-            }
+    install(StatusPages) {
+        exception<AuthenticationException> {
+            call.respondText(
+                "No authorized",
+                contentType = ContentType.Text.Plain,
+                status = HttpStatusCode.Unauthorized
+            )
         }
     }
 }
 
-private suspend inline fun Application.assertAccess(call: ApplicationCall): Boolean {
-    val session = call.sessions.get<SessionKey>() ?: SessionKey("")
-
-    if (!checkKey(session.key)) {
-        val key = if (session.key.isEmpty()) "<empty>" else session.key
-
-        call.respondText("No authorized", contentType = ContentType.Text.Plain)
-        log.warn("Attempt to access with invalid key: $key")
-
-        return false
-    }
-    return true
-}
-
-private fun checkKey(value: String) = value == System.getenv("ktor_private_key")
-
-data class SessionKey(val key: String)
-
-class AuthenticationException : RuntimeException()
-class AuthorizationException : RuntimeException()
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
