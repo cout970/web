fd78f5facca08d33bc975d932e9c5c43c8186b22
cout970 <thecout970@gmail.com>
2019-03-19 17:50:40 +0100

    Add file logging

--- src/Application.kt
+++ src/Application.kt
@@ -10,7 +10,6 @@ import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.content.resources
 import io.ktor.http.content.static
-import io.ktor.request.path
 import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
@@ -45,8 +44,7 @@ fun Application.module(testing: Boolean = false) {
     install(AutoHeadResponse)
 
     install(CallLogging) {
-        level = Level.INFO
-        filter { call -> call.request.path().startsWith("/") }
+        level = Level.DEBUG
     }
 
     install(ConditionalHeaders)
