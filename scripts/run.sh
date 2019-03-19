f7e001dbd5fbbdc60858d506070259496f06c0d8
cout970 <thecout970@gmail.com>
2019-03-19 17:55:34 +0100

    Force to set secret key

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,9 +4,15 @@ if [[ `pwd` == */scripts ]]; then
     cd ..
 fi
 
+# Define secret key
+echo "Enter secret key: "
+read key
+export ktor_private_key=${key}
+echo "Key '${ktor_private_key}'"
+
 runCommand='java -jar build/libs/web.jar -port=80'
 
-until 1; do
+while echo "Starting..."; do
     ${runCommand}
     echo "Server crashed with exit code $?. Respawning in 1 second.." >&2
     sleep 1
